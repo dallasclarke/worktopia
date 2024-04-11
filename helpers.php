373eb8ce3fda8df454814813c2fdf49920a181df
Dallas Clarke <dallas.ag.clarke@gmail.com>
2024-04-10 22:15:47 -0700

    initial

--- helpers.php
+++ helpers.php
@@ -23,3 +23,15 @@ function loadPartial($name){
     echo "Partial '{$name} not found.'";
   }
 }
+
+function inspect($value){
+  echo '<pre>';
+  var_dump($value);
+  echo '</pre>';
+}
+
+function inspectAndDie($value){
+  echo '<pre>';
+  die(var_dump($value));
+  echo '</pre>';
+}
